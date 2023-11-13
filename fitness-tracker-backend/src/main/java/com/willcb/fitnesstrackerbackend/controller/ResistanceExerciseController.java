aba42d5e72648b465b4c1b688976c0b2f1c8315c
Will Barker <willbarker34@gmail.com>
2023-11-13 15:32:08 -0500

    created controllers for models

---
+++ fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/controller/ResistanceExerciseController.java
@@ -0,0 +1,9 @@
+package com.willcb.fitnesstrackerbackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ResistanceExerciseController {
+    
+}
