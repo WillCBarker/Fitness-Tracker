8ee91434fec010a230bee564c7d2d6dcf9b3950d
Will Barker <willbarker34@gmail.com>
2023-11-15 16:11:28 -0500

    exercise endpoint configuration

--- fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/controller/ExerciseController.java
+++ fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/controller/ExerciseController.java
@@ -1,9 +1,80 @@
 package com.willcb.fitnesstrackerbackend.controller;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.willcb.fitnesstrackerbackend.model.Exercise;
+import com.willcb.fitnesstrackerbackend.service.ExerciseService;
 
 
 @RestController
 public class ExerciseController {
-    
+
+    @Autowired
+    private ExerciseService ExerciseService;
+
+    @GetMapping
+    public List<Exercise> getAllExercises() {
+        // need service method
+        return ExerciseService.getAllExercises();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Exercise> getExerciseByID(@PathVariable Long id) {
+
+        try {
+            Exercise exercise = ExerciseService.getExerciseByID(id);
+            return ResponseEntity.ok(exercise);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Exercise> createExercise(@RequestBody Exercise exercise) {
+
+        try {
+            Exercise createdExercise = ExerciseService.createExercise(exercise);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdExercise);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Exercise> updateExercise(@PathVariable Long id, @RequestBody Exercise updatedExercise) {
+
+        try {
+            Exercise exercise = ExerciseService.updateExercise(id, updatedExercise);
+            return ResponseEntity.ok(exercise);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteExercise(@PathVariable Long id) {
+        boolean deleted = ExerciseService.deleteExercise(id);
+
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/service/ExerciseService.java
+++ fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/service/ExerciseService.java
@@ -1,6 +1,11 @@
 package com.willcb.fitnesstrackerbackend.service;
 
+import com.willcb.fitnesstrackerbackend.model.Exercise;
 import com.willcb.fitnesstrackerbackend.repository.ExerciseRepository;
+
+import java.util.*;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +15,42 @@ public class ExerciseService {
     @Autowired
     private ExerciseRepository ExerciseRepository;
 
+        private void validateExercise(Exercise exercise) {
+        // Validate exercise input
+        if (exercise == null || exercise.getName() == null) {
+            throw new IllegalArgumentException("Exercise details are incomplete");
+        }
+    }
+
+    public Exercise getExerciseByID(Long exerciseID){
+        return ExerciseRepository.findById(exerciseID).orElse(null);
+    }
+
+    public List<Exercise> getAllExercises() {
+        return ExerciseRepository.findAll();
+    }
+
+    public Exercise createExercise(Exercise exercise) {
+        validateExercise(exercise);
+
+        return ExerciseRepository.save(exercise);
+    }
+
+    public Exercise updateExercise(Long id, Exercise updatedExercise) {
+        validateExercise(updatedExercise);
+
+        Exercise existingExercise = ExerciseRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Exercise not found"));
+
+        return ExerciseRepository.save(existingExercise);
+    }
+
+    public boolean deleteExercise(Long id) {
+        if (ExerciseRepository.existsById(id)) {
+            ExerciseRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
