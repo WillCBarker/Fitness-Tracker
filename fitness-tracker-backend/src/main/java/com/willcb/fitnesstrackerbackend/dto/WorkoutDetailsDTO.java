5d86a036581ad581a209e6f945c3a1f9e11f2a3a
Will Barker <willbarker34@gmail.com>
2023-11-15 16:13:16 -0500

    moved DTOs to new folder

---
+++ fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/dto/WorkoutDetailsDTO.java
@@ -0,0 +1,32 @@
+package com.willcb.fitnesstrackerbackend.controller.dto;
+
+import java.time.DayOfWeek;
+
+import com.willcb.fitnesstrackerbackend.model.Workout;
+
+public class WorkoutDetailsDTO {
+
+    private DayOfWeek dayOfWeek;
+    private Workout workout;
+
+    public WorkoutDetailsDTO(DayOfWeek dayOfWeek, Workout workout) {
+        this.dayOfWeek = dayOfWeek;
+        this.workout = workout;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public Workout getWorkout() {
+        return workout;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public void setWorkout(Workout workout) {
+        this.workout = workout;
+    }
+}
