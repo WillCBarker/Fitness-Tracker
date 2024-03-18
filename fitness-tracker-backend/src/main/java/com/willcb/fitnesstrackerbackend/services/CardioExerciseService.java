8ccb8f0a71146730cc65005a8953398f654814b4
Will Barker <willbarker34@gmail.com>
2024-03-18 17:11:11 -0400

    made cardioExerciseRepository field immutable

--- fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/services/CardioExerciseService.java
+++ fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/services/CardioExerciseService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CardioExerciseService {
 
-    private CardioExerciseRepository cardioExerciseRepository;
+    private final CardioExerciseRepository cardioExerciseRepository;
 
     @Autowired
     public CardioExerciseService(CardioExerciseRepository cardioExerciseRepository) {
