a8728d464acfa99da11b2047dd31498dd696e0ce
Will Barker <willbarker34@gmail.com>
2024-03-19 14:10:23 -0400

    added method descriptions and refactored wiring to the constructor

--- fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/controllers/WorkoutPlanController.java
+++ fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/controllers/WorkoutPlanController.java
@@ -6,33 +6,41 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.willcb.fitnesstrackerbackend.dto.WorkoutDetailsDTO;
 import com.willcb.fitnesstrackerbackend.entities.WorkoutPlan;
 import com.willcb.fitnesstrackerbackend.services.WorkoutPlanService;
 
-
 @RestController
 @RequestMapping("/workoutplans")
 public class WorkoutPlanController {
-    
-    @Autowired
-    private WorkoutPlanService workoutPlanService;
 
+    private final WorkoutPlanService workoutPlanService;
+
+    @Autowired
+    public WorkoutPlanController(WorkoutPlanService workoutPlanService) {
+        this.workoutPlanService = workoutPlanService;
+    }
+
+    /**
+     * Retrieves all workout plans.
+     * 
+     * @return List<WorkoutPlan> The list of all workout plans.
+     */
     @GetMapping
     public List<WorkoutPlan> getAllWorkoutPlans() {
-        // need service method
         return workoutPlanService.getAllWorkoutPlans();
     }
 
+    /**
+     * Retrieves a workout plan by its ID.
+     * 
+     * @param id The ID of the workout plan to retrieve.
+     * @return ResponseEntity<WorkoutPlan> The ResponseEntity containing the workout plan if found,
+     *         a bad request response if the provided ID is invalid,
+     *         or a not found response if the workout plan with the specified ID does not exist.
+     */
     @GetMapping("/{id}")
     public ResponseEntity<WorkoutPlan> getWorkoutPlanByID(@PathVariable Long id) {
 
@@ -46,6 +54,13 @@ public class WorkoutPlanController {
         }
     }
 
+    /**
+     * Retrieves workouts for a workout plan by its ID.
+     * 
+     * @param id The ID of the workout plan to retrieve workouts for.
+     * @return ResponseEntity<List<WorkoutDetailsDTO>> The ResponseEntity containing the list of workout details if found,
+     *         or a not found response if the workout plan with the specified ID does not exist or has no workouts.
+     */
     @GetMapping("/{id}/workouts")
     public ResponseEntity<List<WorkoutDetailsDTO>> getWorkoutsByPlanId(@PathVariable Long id) {
         try {
@@ -61,6 +76,13 @@ public class WorkoutPlanController {
         }
     }
 
+    /**
+     * Creates a new workout plan.
+     * 
+     * @param workoutPlan The workout plan object to be created.
+     * @return ResponseEntity<WorkoutPlan> The ResponseEntity containing the created workout plan if successful,
+     *         or a bad request response if the provided data is invalid.
+     */
     @PostMapping
     public ResponseEntity<WorkoutPlan> createWorkoutPlan(@RequestBody WorkoutPlan workoutPlan) {
 
@@ -72,6 +94,15 @@ public class WorkoutPlanController {
         }
     }
 
+    /**
+     * Updates an existing workout plan with the specified ID.
+     * 
+     * @param id The ID of the workout plan to be updated.
+     * @param updatedWorkoutPlan The updated information of the workout plan.
+     * @return ResponseEntity<WorkoutPlan> The ResponseEntity containing the updated workout plan if successful,
+     *         a bad request response if the provided data is invalid,
+     *         or a not found response if the workout plan with the specified ID does not exist.
+     */
     @PutMapping("/{id}")
     public ResponseEntity<WorkoutPlan> updateWorkoutPlan(@PathVariable Long id, @RequestBody WorkoutPlan updatedWorkoutPlan) {
 
@@ -85,6 +116,13 @@ public class WorkoutPlanController {
         }
     }
 
+    /**
+     * Deletes an existing workout plan with the specified ID.
+     * 
+     * @param id The ID of the workout plan to be deleted.
+     * @return ResponseEntity<Void> The ResponseEntity indicating success if the workout plan is deleted,
+     *         or a not found response if the workout plan with the specified ID does not exist.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteWorkoutPlan(@PathVariable Long id) {
         boolean deleted = workoutPlanService.deleteWorkoutPlan(id);
