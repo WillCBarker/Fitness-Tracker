cfda0fa0d33168ce127beb8fabdb42d515368f65
Will Barker <willbarker34@gmail.com>
2024-03-19 13:25:17 -0400

    injecting personService via constructor, removed workoutplan endpoint

--- fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/controllers/PersonController.java
+++ fitness-tracker-backend/src/main/java/com/willcb/fitnesstrackerbackend/controllers/PersonController.java
@@ -1,13 +1,6 @@
 package com.willcb.fitnesstrackerbackend.controllers;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,43 +8,32 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import com.willcb.fitnesstrackerbackend.entities.Person;
-import com.willcb.fitnesstrackerbackend.entities.WorkoutPlan;
 import com.willcb.fitnesstrackerbackend.services.PersonService;
 
 
 @RestController
 @RequestMapping("/persons")
 public class PersonController {
-    
+
+    private final PersonService personService;
+
     @Autowired
-    private PersonService personService;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
 
     @GetMapping
     public List<Person> getAllPersons() {
         // need service method
-        return personService.getAllPersons();
+        return this.personService.getAllPersons();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Person> getPersonByID(@PathVariable Long id) {
 
         try {
-            Person person = personService.getPersonByID(id);
+            Person person = this.personService.getPersonByID(id);
             return ResponseEntity.ok(person);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().build();
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.notFound().build();
-        }
-    }
-    
-    @GetMapping("/{id}/workoutplan")
-    public ResponseEntity<WorkoutPlan> getWorkoutPlanByPersonID(@PathVariable Long id) {
-        
-        try {
-            Person person = personService.getPersonByID(id);
-            WorkoutPlan workoutPlan = person.getWorkoutPlan();
-            return ResponseEntity.ok(workoutPlan);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
         } catch (NoSuchElementException e) {
@@ -63,7 +45,7 @@ public class PersonController {
     public ResponseEntity<Person> createPerson(@RequestBody Person person) {
 
         try {
-            Person createdPerson = personService.createPerson(person);
+            Person createdPerson = this.personService.createPerson(person);
             return ResponseEntity.status(HttpStatus.CREATED).body(createdPerson);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
@@ -74,7 +56,7 @@ public class PersonController {
     public ResponseEntity<Person> updatePerson(@PathVariable Long id, @RequestBody Person updatedPerson) {
 
         try {
-            Person person = personService.updatePerson(id, updatedPerson);
+            Person person = this.personService.updatePerson(id, updatedPerson);
             return ResponseEntity.ok(person);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
@@ -85,7 +67,7 @@ public class PersonController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePerson(@PathVariable Long id) {
-        boolean deleted = personService.deletePerson(id);
+        boolean deleted = this.personService.deletePerson(id);
 
         if (deleted) {
             return ResponseEntity.noContent().build();
