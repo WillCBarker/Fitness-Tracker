c252b561e9ee4fabd7ac9d2ba2d6fe0e082870ea
Will Barker <willbarker34@gmail.com>
2024-03-25 14:13:49 -0400

    moved controller tests to controller folder to match project folder structure

---
+++ fitness-tracker-backend/src/test/java/com/willcb/fitnesstrackerbackend/controllers/WorkoutPlanControllerTest.java
@@ -0,0 +1,28 @@
+package com.willcb.fitnesstrackerbackend.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WorkoutPlanControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getAllWorkoutPlansSuccess() throws Exception {
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/workoutplans")
+                .contentType(MediaType.APPLICATION_JSON));
+
+        resultActions.andExpect(status().isOk());
+    }
+}
